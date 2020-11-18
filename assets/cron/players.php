60efb06c763a27eba816238fe55c63d3a60b65cb
Marlon G <35308625+uhmarlon@users.noreply.github.com>
2020-11-18 18:37:08 +0100

    Update players.php

--- assets/cron/players.php
+++ assets/cron/players.php
@@ -1,32 +1,23 @@
 <?php
-//                                                                             //
-// https://github.com/slashchv/PHP-Simple-Query-connected-players-FiveM-Server //
+//                        Developed by slashchv                                //
+//     github.com/slashchv/PHP-Simple-Query-connected-players-FiveM-Server     //
 //                                                                             //
 
 function FivemQueryPlayers($IPServer, $PortServer) {
-    
     $opts = array('http' =>
     array(
         'method'  => 'GET',
-        'timeout' => 2 // Timeout GET
-    )
-    );
+        'timeout' => 2 // Timeout GET 
+    ));
    $context  = stream_context_create($opts);
    $content_info = json_decode(@file_get_contents("http://".$IPServer.":".$PortServer."/info.json", false, $context), true);
-   
     if ($content_info) {
         $gta5_players = @file_get_contents("http://".$IPServer.":".$PortServer."/players.json", false, $context);
         $content_players = json_decode($gta5_players, true);
         $pl_count = count($content_players);
-        
         return $pl_count;
-
     } else {
-        
         return;
-
     }
-
 }
-
 ?>
